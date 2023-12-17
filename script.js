baa2fd857527ba3c660121be9cf7b37f9dda467e
SUDARSAN KS <145418449+Sudarsan-ks@users.noreply.github.com>
2023-12-17 12:17:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function foo(){
+    console.log(document.getElementById("firstname").value);
+    console.log(document.getElementById("middlename").value);
+    console.log(document.getElementById("lastname").value);
+    console.log(document.getElementById("email").value);
+};
+
